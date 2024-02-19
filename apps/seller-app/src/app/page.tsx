2a259ea0547d345cc23f5c650d8be4f325c9f4f1
Enol Casielles <enolcasielles@gmail.com>
2024-02-19 19:29:47 +0100

    commit

--- apps/seller-app/src/app/page.tsx
+++ apps/seller-app/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home (): ReactNode {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
-        <Button className='text-3xl'>Hola botón</Button>
+        <Button variant="default" className='text-3xl'>Hola botón</Button>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
--- packages/design-system/src/components/Button/Button.tsx
+++ packages/design-system/src/components/Button/Button.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Button as ShadcnButton } from '../ui/button'
 
+export const buttonVariants = ['default', 'destructive', 'outline', 'secondary', 'ghost', 'link'] as const
+
+export type ButtonVariant = typeof buttonVariants[number]
 interface Props {
+  variant: ButtonVariant
   className: string
   children: React.ReactNode
 }
 
-export const Button = ({ className, children }: Props): React.ReactNode => {
+export const Button = ({ variant, className, children }: Props): React.ReactNode => {
+  console.log('Button!!!')
   return (
-    <ShadcnButton className={className}>{children}</ShadcnButton>
+    <ShadcnButton variant={variant} className={className}>{children}</ShadcnButton>
   )
 }
